cae38746ba17370faf153cf382d0140b686fecba
kddinev18 <kddinev18@codingburgas.bg>
2024-04-18 19:39:52 +0300

    Add project files.

---
+++ DragonC.CLI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DragonC.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
