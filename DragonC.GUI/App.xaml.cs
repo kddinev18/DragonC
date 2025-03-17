1cf2f9da1004ede3c75ce31d1695d6e6d176346b
kddinev18 <kddinev18@codingburgas.bg>
2025-03-17 20:04:17 +0200

    Add secrets

--- DragonC.GUI/App.xaml.cs
+++ DragonC.GUI/App.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace DragonC.GUI
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DragonC.GUI
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NMaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXxfd3VVQ2VeV0F3W0c=");
             MainPage = new AppShell();
         }
     }
